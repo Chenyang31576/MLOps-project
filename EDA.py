f56d69bfc888da51dd2729c35bc80a8a0331ee87
bipt-lyle <2645385939@qq.com>
2024-02-06 00:54:24 +0100

    Yingda

---
+++ EDA.py
@@ -0,0 +1,206 @@
+
+
+import pandas as pd
+import numpy as np
+import glob
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.graph_objects as go
+from pathlib import Path
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import LSTM, Dense, Dropout
+import csv
+
+
+# In[2]:
+
+
+# Define the folder path where the CSV files are located
+folder_path = r'C:\Users\33766\Desktop\mlipproject\data'
+
+# Use the glob module to find all CSV files in the folder
+csv_files = glob.glob(f'{folder_path}/*.csv')
+
+# Define the column names that you want to keep
+columns_to_keep = ['annee_numero_de_tirage', 'boule_1', 'boule_2', 'boule_3', 'boule_4', 'boule_5', 'etoile_1', 'etoile_2']
+
+# Initialize an empty DataFrame to hold data from all files
+df = pd.DataFrame()
+
+# Iterate over each CSV file
+for file in csv_files:
+    # Read the CSV file, only keeping specified columns and setting encoding, separator, and index column parameters
+    df_temp = pd.read_csv(file, encoding='ISO-8859-1', sep=';', index_col=False, usecols=columns_to_keep)
+    # Append the data to the combined DataFrame
+    df = pd.concat([df, df_temp], ignore_index=True)
+
+# Sort the combined DataFrame by 'annee_numero_de_tirage' in ascending order
+df.sort_values(by='annee_numero_de_tirage', inplace=True)
+# Reset the index of the combined DataFrame
+df.reset_index(drop=True, inplace=True)
+
+
+# In[3]:
+
+
+df
+
+
+# In[4]:
+
+
+# 计算基本统计量
+stats = df.describe()
+print(stats)
+
+
+# #### 每个球频率
+
+# In[5]:
+
+
+# 分开绘制主球号的频率分布
+for i in range(1, 6):  # 对于主球号
+    fig = go.Figure()
+    fig.add_trace(go.Histogram(x=df[f'boule_{i}'], nbinsx=50, marker_color='blue', name=f'boule_{i}'))
+    fig.update_layout(
+        title_text=f'Frequency of boule_{i}',
+        xaxis_title_text='Number',
+        yaxis_title_text='Frequency',
+        bargap=0.2,  # 间距
+    )
+    fig.show()
+
+# 分开绘制星号的频率分布
+for i in range(1, 3):  # 对于星号
+    fig = go.Figure()
+    fig.add_trace(go.Histogram(x=df[f'etoile_{i}'], nbinsx=12, marker_color='red', name=f'etoile_{i}'))
+    fig.update_layout(
+        title_text=f'Frequency of etoile_{i}',
+        xaxis_title_text='Number',
+        yaxis_title_text='Frequency',
+        bargap=0.2,  # 间距
+    )
+    fig.show()
+
+
+# #### 啥也不是热力图
+
+# In[6]:
+
+
+# 创建数字组合的频率矩阵
+cross_tab = pd.crosstab(index=df['boule_1'], columns=df['boule_2'])
+
+# 绘制热图
+plt.figure(figsize=(10, 8))
+sns.heatmap(cross_tab, cmap='viridis')
+plt.title('Heatmap of Number Combinations for boule_1 and boule_2')
+plt.show()
+
+
+# ##### 相关性矩阵
+
+# In[7]:
+
+
+cross_tab = pd.crosstab(index=df['boule_1'], columns=[df['boule_2'], df['boule_3'], df['boule_4'], df['boule_5']])
+
+# 计算五个主球之间的相关性矩阵
+corr_matrix = df[['boule_1', 'boule_2', 'boule_3', 'boule_4', 'boule_5']].corr()
+
+# 绘制热力图
+plt.figure(figsize=(10, 8))
+sns.heatmap(corr_matrix, annot=True, cmap='viridis', fmt=".2f")
+plt.title('Heatmap of Correlation Between Main Balls')
+plt.show()
+
+
+# In[8]:
+
+
+# 创建数字组合的频率矩阵
+cross_tab = pd.crosstab(index=df['etoile_1'], columns=df['etoile_2'])
+
+# 绘制热图
+plt.figure(figsize=(10, 8))
+sns.heatmap(cross_tab, cmap='viridis')
+plt.title('Heatmap of Number Combinations for boule_1 and boule_2')
+plt.show()
+
+
+# #### 绘制每年平均号码的变化趋势
+
+# In[9]:
+
+
+# 假设 'annee_numero_de_tirage' 是年份+编号的格式，先提取年份
+df['year'] = df['annee_numero_de_tirage'].apply(lambda x: str(x)[:4])
+
+# 计算每年的平均号码
+average_numbers_per_year = df.groupby('year').mean()
+
+# 绘制每年平均号码的变化趋势
+plt.figure(figsize=(14, 7))
+sns.lineplot(data=average_numbers_per_year[['boule_1', 'boule_2', 'boule_3', 'boule_4', 'boule_5', 'etoile_1', 'etoile_2']])
+plt.title('Average Lottery Numbers per Year')
+plt.ylabel('Average Number')
+plt.xlabel('Year')
+plt.show()
+
+
+# #### 绘制每年最多出现号码的变化趋势
+
+# In[10]:
+
+
+# 初始化存储每年最多出现号码的 DataFrame
+most_frequent_numbers_per_year = pd.DataFrame()
+
+# 对每个球号进行操作
+for ball in ['boule_1', 'boule_2', 'boule_3', 'boule_4', 'boule_5', 'etoile_1', 'etoile_2']:
+    # 初始化一个列表来存储每年最频繁的数字
+    most_frequent_numbers = []
+    
+    # 对每个年份进行操作
+    for year in df['year'].unique():
+        # 筛选当前年份的数据
+        year_df = df[df['year'] == year]
+        
+        # 计算当前年份当前球号的数字频率，并找到频率最高的数字
+        most_frequent_number = year_df[ball].value_counts().idxmax()
+        
+        # 将最频繁的数字添加到列表中
+        most_frequent_numbers.append(most_frequent_number)
+    
+    # 将每年最频繁的数字加入到 DataFrame 中
+    most_frequent_numbers_per_year[ball] = most_frequent_numbers
+
+# 设置年份为索引
+most_frequent_numbers_per_year.index = df['year'].unique()
+
+# 绘制每年最频繁出现的数字的变化趋势
+plt.figure(figsize=(14, 7))
+for ball in most_frequent_numbers_per_year.columns:
+    sns.lineplot(data=most_frequent_numbers_per_year[ball], label=ball)
+plt.title('Most Frequent Lottery Numbers per Year')
+plt.ylabel('Most Frequent Number')
+plt.xlabel('Year')
+plt.legend(title='Ball Number')
+plt.xticks(rotation=45)  # 旋转 x 轴标签以便更好地显示
+plt.show()
+
+
+# #### 检测异常值
+
+# In[11]:
+
+
+# 使用箱线图来检测每个数字的潜在异常值
+plt.figure(figsize=(14, 7))
+sns.boxplot(data=df[['boule_1', 'boule_2', 'boule_3', 'boule_4', 'boule_5', 'etoile_1', 'etoile_2']])
+plt.title('Boxplot of Lottery Numbers')
+plt.show()
