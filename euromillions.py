2fad645caa45a1597259be748717d5c26776e22d
bipt-lyle <2645385939@qq.com>
2024-02-06 23:31:08 +0100

    ceshi

--- euromillions.py
+++ euromillions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[56]:
 
 
 import pandas as pd
@@ -16,14 +16,17 @@ from sklearn.preprocessing import StandardScaler
 from keras.models import Sequential
 from keras.models import load_model
 from keras.layers import LSTM, Dense, Dropout
-import csv
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
 
 
-# In[2]:
+# In[57]:
 
 
 # Define the folder path where the CSV files are located
 folder_path = r'C:\Users\33766\Desktop\mlipproject\data'
+# folder_path = r'C:\Users\33766\Desktop\mlipproject\data'
 
 # Use the glob module to find all CSV files in the folder
 csv_files = glob.glob(f'{folder_path}/*.csv')
@@ -47,13 +50,13 @@ df.sort_values(by='annee_numero_de_tirage', inplace=True)
 df.reset_index(drop=True, inplace=True)
 
 
-# In[3]:
+# In[58]:
 
 
 df
 
 
-# In[4]:
+# In[59]:
 
 
 # 计算基本统计量
@@ -63,7 +66,7 @@ print(stats)
 
 # #### 每个球频率
 
-# In[5]:
+# In[60]:
 
 
 # 分开绘制主球号的频率分布
@@ -93,7 +96,7 @@ for i in range(1, 3):  # 对于星号
 
 # #### 啥也不是热力图
 
-# In[6]:
+# In[61]:
 
 
 # 创建数字组合的频率矩阵
@@ -108,7 +111,7 @@ plt.show()
 
 # ##### 相关性矩阵
 
-# In[7]:
+# In[62]:
 
 
 cross_tab = pd.crosstab(index=df['boule_1'], columns=[df['boule_2'], df['boule_3'], df['boule_4'], df['boule_5']])
@@ -123,7 +126,7 @@ plt.title('Heatmap of Correlation Between Main Balls')
 plt.show()
 
 
-# In[8]:
+# In[63]:
 
 
 # 创建数字组合的频率矩阵
@@ -138,7 +141,7 @@ plt.show()
 
 # #### 绘制每年平均号码的变化趋势
 
-# In[9]:
+# In[64]:
 
 
 # 假设 'annee_numero_de_tirage' 是年份+编号的格式，先提取年份
@@ -158,7 +161,7 @@ plt.show()
 
 # #### 绘制每年最多出现号码的变化趋势
 
-# In[10]:
+# In[65]:
 
 
 # 初始化存储每年最多出现号码的 DataFrame
@@ -200,7 +203,7 @@ plt.show()
 
 # #### 检测异常值
 
-# In[11]:
+# In[66]:
 
 
 # 使用箱线图来检测每个数字的潜在异常值
@@ -210,113 +213,188 @@ plt.title('Boxplot of Lottery Numbers')
 plt.show()
 
 
-# #### 预测模型
+# #### 标准化 划分训练集 测试集
 
-# In[12]:
-
-
-df = df.drop(['annee_numero_de_tirage','year'], axis=1)
-df.head()
+# In[67]:
 
 
-# In[13]:
+df = df.drop(['annee_numero_de_tirage'
+              ,'year'
+             ], axis=1)
+df
 
 
+# In[68]:
+
+
+#标准化
 scaler = StandardScaler().fit(df.values)
 transformed_dataset = scaler.transform(df.values)
 transformed_df = pd.DataFrame(data=transformed_dataset, index=df.index)
-transformed_df.head()
+print(transformed_df.head)
+
+number_of_rows = df.values.shape[0]
+window_length = 5
+number_of_features = df.values.shape[1]
 
 
-# In[14]:
+# In[69]:
 
 
-number_of_rows= df.values.shape[0] #all our games
-window_length = 5 #amount of past games we need to take in consideration for prediction
-number_of_features = df.values.shape[1] #balls count
+# 划分训练集、测试集和验证集
+train_size = int(number_of_rows * 0.8)
+test_size = int((number_of_rows - train_size) * 0.5)  # 50% 划分给测试集，50% 划分给验证集
+
+train_data, test_val_data = transformed_df.iloc[:train_size], transformed_df.iloc[train_size:]
+test_data, val_data = train_test_split(test_val_data, test_size=test_size, shuffle=False)
+
+# 构建训练集和标签集
+train = np.empty([train_size - window_length, window_length, number_of_features], dtype=float)
+label = np.empty([train_size - window_length, number_of_features], dtype=float)
+
+for i in range(0, train_size - window_length):
+    train[i] = train_data.iloc[i:i+window_length, 0:number_of_features]
+    label[i] = train_data.iloc[i+window_length:i+window_length+1, 0:number_of_features]
+
+# 构建测试集和标签集
+test = np.empty([test_size - window_length, window_length, number_of_features], dtype=float)
+test_label = np.empty([test_size - window_length, number_of_features], dtype=float)
+
+for i in range(0, test_size - window_length):
+    test[i] = test_data.iloc[i:i+window_length, 0:number_of_features]
+    test_label[i] = test_data.iloc[i+window_length:i+window_length+1, 0:number_of_features]
+
+# 构建验证集和标签集
+val = np.empty([len(val_data) - window_length, window_length, number_of_features], dtype=float)
+val_label = np.empty([len(val_data) - window_length, number_of_features], dtype=float)
+
+for i in range(0, len(val_data) - window_length):
+    val[i] = val_data.iloc[i:i+window_length, 0:number_of_features]
+    val_label[i] = val_data.iloc[i+window_length:i+window_length+1, 0:number_of_features]
 
 
-# In[15]:
+# #### 建立模型
+
+# In[70]:
 
 
-train = np.empty([number_of_rows-window_length, window_length, number_of_features], dtype=float)
-label = np.empty([number_of_rows-window_length, number_of_features], dtype=float)
-
-for i in range(0, number_of_rows-window_length):
-    train[i]=transformed_df.iloc[i:i+window_length, 0: number_of_features]
-    label[i]=transformed_df.iloc[i+window_length: i+window_length+1, 0: number_of_features]
-
-
-# In[16]:
-
-
-train.shape
-
-
-# In[17]:
-
-
-label.shape
-
-
-# In[18]:
-
-
-train[0]
-
-
-# In[19]:
-
-
-train[1]
-
-
-# In[20]:
-
-
-label[0]
-
-
-# In[21]:
-
-
-label[1]
-
-
-# In[22]:
-
-
+#模型
 batch_size = 25 
 if os.path.exists('/Users/dantashashou/Downloads/euromillions/euromillions.h5'):
     model = load_model('/Users/dantashashou/Downloads/euromillions/euromillions.h5')
 else:
     model = Sequential()
-    model.add(LSTM(32,      
-               input_shape=(window_length, number_of_features),
-               return_sequences=True))
+    model.add(LSTM(32, input_shape=(window_length, number_of_features), return_sequences=True))
     model.add(Dropout(0.2))
-    model.add(LSTM(32,           
-               return_sequences=False))
+    model.add(LSTM(32, return_sequences=False))
     model.add(Dropout(0.2))
     model.add(Dense(number_of_features))
+
+    # #自定义损失函数，添加约束惩罚使号码不重复
+    # def custom_loss(y_true, y_pred):
+    #     mse = MeanSquaredError()(y_true, y_pred)  # 均方误差作为基础损失
+
+    #     # 添加对规则的约束惩罚项
+    #     penalty = tf.constant(1e6)  # 惩罚项的权重，可以根据实际情况调整
+
+    #     # 检查前五个主球是否有重复
+    #     repeated_boule_penalty = penalty * tf.reduce_sum(tf.square(tf.reduce_sum(tf.abs(y_pred[:, :5] - tf.expand_dims(y_pred[:, :5], 1)), axis=-1) - 1.0))
+
+    #     # 检查后两个星号是否有重复
+    #     repeated_etoile_penalty = penalty * tf.reduce_sum(tf.square(tf.reduce_sum(tf.abs(y_pred[:, 5:7] - tf.expand_dims(y_pred[:, 5:7], 1)), axis=-1) - 1.0))
+
+    #     return mse + repeated_boule_penalty + repeated_etoile_penalty
+
+        
+
+
+    #模型编译和训练
     model.compile(loss='mse', optimizer='rmsprop')
-    model.fit(train, label, batch_size=64, epochs=100)
-    # model.save('input/'+filename+'.h5')
+    # model.compile(loss=custom_loss, optimizer='rmsprop')
+    history = model.fit(train, label, validation_data=(val, val_label), batch_size=64, epochs=200)
+
+    # 保存模型
+    # model.save('/Users/dantashashou/Downloads/euromillions/euromillions.h5')
 
 
-# In[23]:
+# In[71]:
 
 
-to_predict=df.iloc[-5:]
+# 模型评估可视化
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Mean Squared Error')
+plt.legend()
+plt.show()
+
+
+# In[72]:
+
+
+# 模型预测
+scaled_predicted_output = model.predict(test)
+predicted_output = scaler.inverse_transform(scaled_predicted_output).astype(int)
+
+test_label_original = scaler.inverse_transform(test_label)
+
+# 计算均方根误差
+rmse = np.sqrt(mean_squared_error(test_label, predicted_output))
+
+print("Root Mean Squared Error (RMSE):", rmse)
+
+# 可视化预测结果
+plt.figure(figsize=(10, 6))
+for i in range(number_of_features):
+    plt.subplot(2, 4, i+1)
+    plt.plot(test_label_original[:, i], label='Actual')
+    plt.plot(predicted_output[:, i], label='Predicted')
+    plt.title(f'Feature {i+1}')
+    plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+
+# In[77]:
+
+
+# 计算每个球的预测准确率
+ball_names = ['boule_1', 'boule_2', 'boule_3', 'boule_4', 'boule_5', 'etoile_1', 'etoile_2']
+accuracies = []
+for i in range(number_of_features):
+    correct_predictions = np.sum(test_label_original[:, i] == predicted_output[:, i])
+    total_predictions = len(test_label_original[:, i])
+    accuracy = correct_predictions / total_predictions * 100
+    accuracies.append(accuracy)
+
+# 绘制圆环图
+plt.figure(figsize=(10, 6))
+colors = plt.cm.tab20.colors
+for i, (accuracy, color) in enumerate(zip(accuracies, colors)):
+    plt.barh(i, accuracy, color=color, label=ball_names[i])
+    plt.text(accuracy, i, f'{accuracy:.1f}%', va='center')
+plt.xlim(0, 100)
+plt.yticks(range(number_of_features), ball_names)
+plt.xlabel('Accuracy (%)')
+plt.title('Prediction Accuracy for Each Ball')
+plt.grid(axis='x')
+plt.legend()
+plt.gca().invert_yaxis()  # 翻转y轴，使得球的顺序与输入的顺序一致
+plt.show()
+
+
+
+# In[73]:
+
+
+# 预测部分
+to_predict = df.iloc[-5:]
 scaled_to_predict = scaler.transform(to_predict)
-
-
-# In[24]:
-
 
 scaled_predicted_output_1 = model.predict(np.array([scaled_to_predict]))
 data = scaler.inverse_transform(scaled_predicted_output_1).astype(int)
 predict = pd.DataFrame(data, columns=['boule_1', 'boule_2', 'boule_3', 'boule_4', 'boule_5', 'etoile_1', 'etoile_2'])
-# df.to_csv(''+filename+'.csv', index=False)  
+
 predict
 
